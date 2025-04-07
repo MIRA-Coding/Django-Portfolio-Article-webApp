1dddc6a654b920f886c1e8f099cb1966bc769ef9
Amira <ameramohsen.silver@gmail.com>
2025-04-07 13:29:48 +0300

    connect mariadb and create articles db contain: articles, tag, article_tag .. need to fill it and connect it with html

--- article/models.py
+++ article/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tag, related_name='articles', blank=True)
+
+    def __str__(self):
+        return self.title
